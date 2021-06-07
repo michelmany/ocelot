b33ca2d1fd5ced30857ba6535fd6b317adf60f22
Michel Moraes <michel@nitdesign.com.br>
2021-06-07 17:13:28 +0200

    Header and Hero adjustments

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,29 +1,29 @@
 <header>
+  <section class="navbar">
+    <div class="container">
+      <a class="navbar__logo-wrapper brand">
+        <img src="@asset('images/ocelot-logo.svg')" alt="{{ get_bloginfo('name', 'display') }}" class="navbar__logo">
+      </a>
+      
+      <nav class="nav-primary main-menu d-none d-lg-block">
+        @if ( has_nav_menu('primary_navigation') )
+          {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'list-unstyled d-flex']) !!}
+        @endif
+      </nav>
 
-<section class="navbar">
-  <div class="container">
-    <a class="navbar__logo-wrapper brand">
-      <img src="@asset('images/ocelot-logo.svg')" alt="{{ get_bloginfo('name', 'display') }}" class="navbar__logo">
-    </a>
-    
-    <nav class="nav-primary main-menu d-none d-lg-block">
-      @if ( has_nav_menu('primary_navigation') )
-        {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'list-unstyled d-flex']) !!}
-      @endif
-    </nav>
-
-    <div id="menu-toggle" class="d-lg-none">
-      <span></span>
-      <span></span>
-      <span></span>
+      <div id="menu-toggle" class="d-lg-none">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
     </div>
-  </div>
-</section>
+  </section>
 
-<nav id="menu-mobile">
-  @if ( has_nav_menu('primary_navigation') )
-  {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'list-unstyled']) !!}
-  @endif
-</nav>
-
+  <nav id="menu-mobile" class="d-lg-none">
+    <div class="container">
+      @if ( has_nav_menu('primary_navigation') )
+      {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'list-unstyled']) !!}
+      @endif
+    </div>
+  </nav>
 </header>
